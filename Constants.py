e6aad60e39cfd59e84031f0d14b22ca7ad716c08
Wang <937375857@qq.com>
2023-04-07 15:53:26 +0900

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,13 +1,10 @@
-PAD = 0
-
-DATASET = "Yelp2018"  # Yelp2018  Gowalla  Foursquare  Yelp ml-1M douban-book
+DATASET = "Yelp2018"  # Yelp2018  Gowalla  Foursquare  Yelp douban-book
 ENCODER = 'hGCN'  # hGCN  Transformer  TransformerLS  gMLP
 ABLATION = 'Full'  # Full  w/oImFe  w/oFeTra w/oGlobal w/oAtt w/oConv w/oGraIm
 COLD_START = False  # True, False
 
 user_dict = {
     'Yelp': 30887,
-    'ml-1M': 6038,  # 3533
     'douban-book': 12859,
     'Gowalla': 18737,
     'Yelp2018': 31668,
@@ -16,18 +13,18 @@ user_dict = {
 
 poi_dict = {
     'Yelp': 18995,
-    'ml-1M': 3533,  # 3629
     'douban-book': 22294,
     'Gowalla': 32510,
     'Yelp2018': 38048,
     'Foursquare': 28483
 }
+
 POI_NUMBER = poi_dict.get(DATASET)
 USER_NUMBER = user_dict.get(DATASET)
-
 
 print('Dataset:', DATASET, '#User:', USER_NUMBER, '#POI', POI_NUMBER)
 print('Encoder: ', ENCODER)
 print('ABLATION: ', ABLATION)
 print('COLD_START: ', COLD_START)
 
+PAD = 0

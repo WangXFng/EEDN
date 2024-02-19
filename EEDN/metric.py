01b07f2a1d995b8f8335bdb2b05d296cd95f30e0
Kaysen <937375857@qq.com>
2024-02-19 10:40:06 +0900

    add commits for metrics

--- EEDN/metric.py
+++ EEDN/metric.py
@@ -4,14 +4,15 @@ import Constants as C
 
 
 def precision_recall_ndcg_at_k(k, rankedlist, test_matrix):
-    idcg_k = 0
-    dcg_k = 0
-    map = 0
-    ap = 0
+    idcg_k, dcg_k, map, ap = 0, 0, 0, 0
 
+    # Please check consistency with your baselines.
+    # All baselines and this method followed this setting to compute the ideal DCG through a truncation.
+    # (See Line. 94 (https://github.com/Coder-Yu/SELFRec/blob/main/util/evaluation.py))
     n_k = k if len(test_matrix) > k else len(test_matrix)
     for i in range(n_k):
         idcg_k += 1 / math.log(i + 2, 2)
+
     b1 = rankedlist
     b2 = test_matrix
     s2 = set(b2)
@@ -54,31 +55,3 @@ def pre_rec_top(pre, rec, map_, ndcg, prediction, label, event_type):
 
     for i, topN in enumerate([1, 5, 10, 20]):
         vaild(prediction, label, topN, pre[i], rec[i], map_[i], ndcg[i])
-
-
-# def map_mrr_ndcg(rankedlist, test_matrix):
-#     ap = 0
-#     map = 0
-#     dcg = 0
-#     idcg = 0
-#     mrr = 0
-#     for i in range(len(test_matrix)):
-#         idcg += 1 / math.log(i + 2, 2)
-#
-#     b1 = rankedlist
-#     b2 = test_matrix
-#     s2 = set(b2)
-#     hits = [(idx, val) for idx, val in enumerate(b1) if val in s2]
-#     count = len(hits)
-#
-#     for c in range(count):
-#         ap += (c + 1) / (hits[c][0] + 1)
-#         dcg += 1 / math.log(hits[c][0] + 2, 2)
-#
-#     if count != 0:
-#         mrr = 1 / (hits[0][0] + 1)
-#
-#     if count != 0:
-#         map = ap / count
-#
-#     return map, mrr, float(dcg / idcg)
